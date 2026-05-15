7bd8414ab07811515bc89031c885921903a93edc
Desktop Marcus <marcus.pinheiro@super.ufam.edu.br>
2026-05-14 21:22:32 -0400

    feat: cria card com alternativas

--- app/src/main/java/br/com/marcus/quiz/Quiz.kt
+++ app/src/main/java/br/com/marcus/quiz/Quiz.kt
@@ -3,8 +3,11 @@ package br.com.marcus.quiz
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -35,6 +38,21 @@ fun Quiz() {
             text = question.text,
             style = MaterialTheme.typography.titleLarge
         )
+        Spacer(modifier = Modifier.height(24.dp))
+        question.options.forEachIndexed { index: Int, option: String ->
+            Card(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = option,
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+
+            }
+
+        }
     }
 }
 
