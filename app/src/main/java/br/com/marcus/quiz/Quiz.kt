61937ce5db3a4ee22f44f23467632aa24214bfd5
Desktop Marcus <marcus.pinheiro@super.ufam.edu.br>
2026-05-15 09:59:27 -0400

    feat: adiciona cor as respostas erradas e certas

--- app/src/main/java/br/com/marcus/quiz/Quiz.kt
+++ app/src/main/java/br/com/marcus/quiz/Quiz.kt
@@ -1,5 +1,6 @@
 package br.com.marcus.quiz
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,8 +8,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.marcus.quiz.ui.theme.QuizTheme
@@ -44,6 +48,12 @@ fun Quiz() {
         )
         Spacer(modifier = Modifier.height(24.dp))
         question.options.forEachIndexed { index: Int, option: String ->
+            val borderColor = when {
+                !answered -> Color.LightGray
+                index == question.indexCorrectAnswer -> Color.Green
+                index == selectedOption -> Color.Red
+                else -> Color.LightGray
+            }
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -51,7 +61,15 @@ fun Quiz() {
                         answered = true
                         selectedOption = index
                         if (index == question.indexCorrectAnswer) {score++}
+
                     }
+                    .border(
+                        width = 2.dp,
+                        color = borderColor,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                shape = RoundedCornerShape(16.dp),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp  )
             ) {
                 Text(
                     text = option,
